ee40d74f954825304dcd17b22ba3979bbbeee98c
Bratipah <bratipahmh@gmail.com>
2022-06-27 19:15:48 +0300

    add dashboard&users

---
+++ js/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBD05ye53dBe3nfYmqfgp-MzrBdySCzcVM",
+  authDomain: "finance-mgt-b13bf.firebaseapp.com",
+  projectId: "finance-mgt-b13bf",
+  storageBucket: "finance-mgt-b13bf.appspot.com",
+  messagingSenderId: "1047391809231",
+  appId: "1:1047391809231:web:a6f05e602e766da850d1ba",
+  measurementId: "G-Y0W48J3RLL"
+};
+
+  firebase.initializeApp(firebaseConfig);
